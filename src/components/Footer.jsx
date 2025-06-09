0dc51a5617fb5fe38e3d02f06508f5702bf596a3
Marek Belis <marek.belis810@gmail.com>
2025-06-09 11:22:22 +0200

    fix: white margins top and bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='ftr'>
-        <div className='imgHolder'><img src={`${import.meta.env.BASE_URL}logoMED.svg`} alt="logoMED" className="logoIMG" height={100}/></div>
+        <img src={`${import.meta.env.BASE_URL}logoMED.svg`} alt="logoMED" className="logoIMG" height={100}/>
     <p className='credentials'>©MB 2024/25.</p>
     </div>
   )
